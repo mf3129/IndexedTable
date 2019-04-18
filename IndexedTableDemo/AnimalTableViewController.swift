8fc38b1765c1a90d8ecd5deb71427397b8f30db8
Makan Fofana <mfofanaT99@gmail.com>
2019-04-18 14:55:29 -0400

    Constructed section and rows with images.

--- IndexedTableDemo/AnimalTableViewController.swift
+++ IndexedTableDemo/AnimalTableViewController.swift
@@ -13,30 +13,14 @@ class AnimalTableViewController: UITableViewController {
     var animalsDict = [String: [String]]()
     var animalSectionTitles = [String]()
     
-    func createAnimalDict() {
-        //Retrieving first letter of the animal and building dictionary
-        for animal in animals {
-            let firstLetterIndex = animal.index(animal.startIndex, offsetBy: 1)
-            let animalKey = String(animal[..<firstLetterIndex])
-            
-            if var animalValues = animalsDict[animalKey] {
-                animalValues.append(animal)
-                animalsDict[animalKey] = animalValues }
-            else {
-                animalsDict[animalKey] = [animal]
-            }
-        }
-        
-        //Retrieving section title from animal and sorting it.
-        animalSectionTitles = [String](animalsDict.keys)
-    }
-    
     let animals = ["Bear", "Black Swan", "Buffalo", "Camel", "Cockatoo", "Dog", "Donkey", "Emu", "Giraffe", "Greater Rhea", "Hippopotamus", "Horse", "Koala", "Lion", "Llama", "Manatus", "Meerkat", "Panda", "Peacock", "Pig", "Platypus", "Polar Bear", "Rhinoceros", "Seagull", "Tasmania Devil", "Whale", "Whale Shark", "Wombat"]
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createAnimalDict()
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -54,20 +38,31 @@ class AnimalTableViewController: UITableViewController {
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // Return the number of sections.
-        return 1
+        return animalSectionTitles.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Return the number of rows in the section.
-        return animals.count
+        let animalKey = animalSectionTitles[section]
+        guard let animalValues = animalsDict[animalKey] else {
+            return 0
+        }
+        
+        return animalValues.count
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return animalSectionTitles[section]
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
         // Configure the cell...
-        cell.textLabel?.text = animals[indexPath.row]
+        let animalKey = animalSectionTitles[indexPath.section]
+        if let animalValues = animalsDict[animalKey] {
+            cell.textLabel?.text = animalValues[indexPath.row]
+        }
         
         // Convert the animal name to lower case and
         // then replace all occurences of a space with an underscore
@@ -77,6 +72,26 @@ class AnimalTableViewController: UITableViewController {
         return cell
     }
 
+    
+    //Function Call To Group & Sort in Ascending Order
+    func createAnimalDict() {
+        //Retrieving first letter of the animal and building dictionary
+        for animal in animals {
+            let firstLetterIndex = animal.index(animal.startIndex, offsetBy: 1)
+            let animalKey = String(animal[..<firstLetterIndex])
+            
+            if var animalValues = animalsDict[animalKey] {
+                animalValues.append(animal)
+                animalsDict[animalKey] = animalValues }
+            else {
+                animalsDict[animalKey] = [animal]
+            }
+        }
+        
+        //Retrieving section title from animal and sorting it.
+        animalSectionTitles = [String](animalsDict.keys)
+        animalSectionTitles.sort(by: {$0 < $1})
+    }
 
 }
 
